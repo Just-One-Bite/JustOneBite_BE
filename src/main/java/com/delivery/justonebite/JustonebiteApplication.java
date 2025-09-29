1913fe031a66fdbb4d6e3a80e1e68b0c4136afaa
songkyung <min.songkyung@gmail.com>
2025-09-29 21:05:28 +0900

    Initial commit

---
+++ src/main/java/com/delivery/justonebite/JustonebiteApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.justonebite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustonebiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustonebiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delivery/justonebite/JustonebiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.justonebite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustonebiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
