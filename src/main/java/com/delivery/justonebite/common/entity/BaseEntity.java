f4c2ad8b1821a29b4f7841b0fd713dc927ca417f
WonJin <try3982@kakao.com>
2025-09-30 15:11:16 +0900

    - ustonebiteApplication 클래스에 @EnableJpaAuditing 추가 - Long 타입으로 createdBy,updatedBy, deletedBy 추가

--- src/main/java/com/delivery/justonebite/common/entity/BaseEntity.java
+++ src/main/java/com/delivery/justonebite/common/entity/BaseEntity.java
@@ -6,7 +6,9 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -29,5 +31,16 @@ public abstract class BaseEntity {
 
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private Long createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private Long updatedBy;
+
+    @Column(name = "deleted_by")
+    private Long deletedBy;
 
 }
