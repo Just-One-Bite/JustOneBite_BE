deee5282956189acabb69c0252a6d954777aed42
thdwnsdlf12 <il4500@naver.com>
2025-09-30 19:17:33 +0900

    feat: 유저 엔티티 및 권한 enum 추가

---
+++ src/main/java/com/delivery/justonebite/user/domain/entity/User.java
@@ -0,0 +1,42 @@
+package com.delivery.justonebite.user.domain.entity;
+
+import com.delivery.justonebite.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "h_users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    public User(
+            String email,
+            String name,
+            String password,
+            UserRole userRole) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.userRole = userRole;
+    }
+}
