ab37ba8ef5d57ecd2f86ef0148d87d9d6fc2a210
thdwnsdlf12 <il4500@naver.com>
2025-10-07 02:48:49 +0900

    feat: 유저 프로필 조회 기능 추가

--- src/main/java/com/delivery/justonebite/user/application/service/UserService.java
+++ src/main/java/com/delivery/justonebite/user/application/service/UserService.java
@@ -1,7 +1,11 @@
 package com.delivery.justonebite.user.application.service;
 
+import com.delivery.justonebite.global.common.security.UserDetailsImpl;
+import com.delivery.justonebite.user.domain.entity.User;
 import com.delivery.justonebite.user.domain.repository.UserRepository;
+import com.delivery.justonebite.user.presentation.dto.response.GetProfileResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +13,16 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public GetProfileResponse findMyProfile(UserDetailsImpl userDetails) {
+        User myProfile = userRepository.findByEmail(userDetails.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return GetProfileResponse.toDto(myProfile);
+    }
+
+    // Todo: 유저 프로필 수정
+
+    // Todo: 비밀번호 변경
+
+    // Todo: 회원 탈퇴
 }
--- src/main/java/com/delivery/justonebite/user/presentation/controller/UserController.java
+++ src/main/java/com/delivery/justonebite/user/presentation/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.delivery.justonebite.user.presentation.controller;
 
+import com.delivery.justonebite.global.common.security.UserDetailsImpl;
 import com.delivery.justonebite.user.application.service.UserService;
+import com.delivery.justonebite.user.presentation.dto.response.GetProfileResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<GetProfileResponse> getMyProfile(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        GetProfileResponse myProfile = userService.findMyProfile(userDetails);
+        return ResponseEntity.status(HttpStatus.OK).body(myProfile);
+    }
+
+    // Todo: 유저 프로필 수정
+
+    // Todo: 비밀번호 변경
+
+    // Todo: 회원 탈퇴
 }
