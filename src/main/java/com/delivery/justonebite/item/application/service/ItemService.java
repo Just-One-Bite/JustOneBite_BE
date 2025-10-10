02c1ec87a8ecaf76e00a0c41278d8fc3b2bf7278
Namyejun <yejun1148@gmail.com>
2025-10-10 22:16:47 +0900

    feat: Item 및 Ai Request History 권한 제어 - Item 숨김 및 삭제에 따른 조회 등

--- src/main/java/com/delivery/justonebite/item/application/service/ItemService.java
+++ src/main/java/com/delivery/justonebite/item/application/service/ItemService.java
@@ -13,12 +13,14 @@ import com.delivery.justonebite.item.presentation.dto.ItemRequest;
 import com.delivery.justonebite.item.presentation.dto.ItemUpdateRequest;
 import com.delivery.justonebite.shop.domain.entity.Shop;
 import com.delivery.justonebite.shop.domain.repository.ShopRepository;
+import com.delivery.justonebite.user.domain.entity.UserRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -33,59 +35,17 @@ public class ItemService {
 
     private final AiRequestHistoryRepository aiRequestHistoryRepository;
 
+    // 상품 CREATE
     @Transactional
-    public ItemResponse createItem(ItemRequest request) {
-        Shop shop = shopRepository.findById(UUID.fromString(request.shopId())).orElseThrow(
-            () -> new CustomException(ErrorCode.INVALID_SHOP)
-        );
+    public ItemResponse createItem(Long userId, UserRole role, ItemRequest request) {
+        Shop shop = checkValidRequestWithShop(userId, role, UUID.fromString(request.shopId()));
+
         Item item = request.toItem();
         item.setShop(shop);
       
-        if (request.aiGenerated()) { // 상품 소개 AI API를 통해 작성
-            String response = generateAiResponse(item, request.description());
-
-            // AI 사용 기록 저장
-            saveAiRequestHistory(1L, request.description(), response);
-        } else { // 상품 소개 직접 작성
-            itemRepository.save(item);
-        }
-
-        return ItemResponse.from(item);
-    }
-
-    public ItemDetailResponse getItemFromOwner(UUID itemId) {
-        return ItemDetailResponse.from(itemRepository.findByItemIdWithNativeQuery(itemId).orElseThrow(
-            () -> new CustomException(ErrorCode.INVALID_ITEM)
-        ));
-    }
-
-    public ItemDetailResponse getItemFromCustomer(UUID itemId) {
-        return ItemDetailResponse.from(itemRepository.findByItemId(itemId).orElseThrow(
-            () -> new CustomException(ErrorCode.INVALID_ITEM)
-        ));
-    }
-
-    public Page<ItemResponse> getItemsByShop(UUID shopId, Pageable pageable) { // owner 입장에서의 상품 조회
-        return itemRepository.findAllByShopIdWithNativeQuery(shopId, pageable).map(ItemResponse::from);
-    }
-
-    public Page<ItemResponse> getItemsByShopWithoutHidden(UUID shopId, Pageable pageable) { // customer 입장에서의 상품 조회
-        return itemRepository.findAllByShopIdWithoutHidden(shopId, pageable).map(ItemResponse::from);
-    }
-
-    @Transactional
-    public ItemResponse updateItem(UUID itemId, ItemUpdateRequest request) {
-        Item item = itemRepository.findByItemId(itemId).orElseThrow(
-            () -> new CustomException(ErrorCode.INVALID_ITEM)
-        );
-
-        item.updateItem(request);
-
         if (request.aiGenerated()) {
             String response = generateAiResponse(item, request.description());
-
-            // AI 사용 기록 저장
-            saveAiRequestHistory(1L, request.description(), response);
+            saveAiRequestHistory(userId, request.description(), response);
         } else {
             itemRepository.save(item);
         }
@@ -93,27 +53,72 @@ public class ItemService {
         return ItemResponse.from(item);
     }
 
+    // 숨김 및 삭제 상품 포함 단건 Read
+    public ItemDetailResponse getItemFromOwner(Long userId, UserRole role, UUID itemId) {
+        Item item = checkValidRequestWithItem(userId, role, itemId);
+
+        return ItemDetailResponse.from(item);
+    }
+
+    // 숨김 및 삭제 상품 제외 단건 Read
+    public ItemDetailResponse getItemFromCustomer(UUID itemId) {
+        return ItemDetailResponse.from(itemRepository.findByItemIdWithoutHidden(itemId).orElseThrow(
+            () -> new CustomException(ErrorCode.INVALID_ITEM)
+        ));
+    }
+
+    // 가게별 숨김 및 삭제 상품 포함 Read
+    public Page<ItemResponse> getItemsByShopFromOwner(Long userId, UserRole role, UUID shopId, Pageable pageable) {
+        checkValidRequestWithShop(userId, role, shopId);
+
+        return itemRepository.findAllByShopIdWithNativeQuery(shopId, pageable).map(ItemResponse::from);
+    }
+
+    // 가게별 숨김 및 삭제 상품 제외 Read
+    public Page<ItemResponse> getItemsByShopFromCustomer(UUID shopId, Pageable pageable) { // customer 입장에서의 상품 조회
+        return itemRepository.findAllByShopIdWithoutHidden(shopId, pageable).map(ItemResponse::from);
+    }
+
+    // 상품 Update
     @Transactional
-    public void softDelete(Long deleterId, UUID itemId) {
-        Item item = itemRepository.findByItemId(itemId).orElseThrow(() -> new CustomException(ErrorCode.INVALID_ITEM));
-        item.softDelete(deleterId);
+    public ItemResponse updateItem(Long userId, UserRole role, UUID itemId, ItemUpdateRequest request) {
+        Item item = checkValidRequestWithItem(userId, role, itemId);
+
+        item.updateItem(request);
+
+        if (request.aiGenerated()) {
+            String response = generateAiResponse(item, request.description());
+            saveAiRequestHistory(userId, request.description(), response);
+        } else {
+            itemRepository.save(item);
+        }
+
+        return ItemResponse.from(item);
+    }
+
+    // 상품 Soft Delete
+    @Transactional
+    public void softDelete(Long userId, UserRole role, UUID itemId) {
+        Item item = checkValidRequestWithItem(userId, role, itemId);
+
+        item.softDelete(userId);
         itemRepository.save(item);
     }
 
+    // 상품 Restore
     @Transactional
-    public void restoreItem(UUID itemId) {
-        Item item = itemRepository.findByItemIdWithNativeQuery(itemId).orElseThrow(
-            () -> new CustomException(ErrorCode.INVALID_ITEM)
-        );
+    public void restoreItem(Long userId, UserRole role, UUID itemId) {
+        Item item = checkValidRequestWithItem(userId, role, itemId);
+
         item.restore();
         itemRepository.save(item);
     }
 
+    // 상품 Hidden / Reveal
     @Transactional
-    public void toggleHidden(UUID itemId) {
-        Item item = itemRepository.findByItemId(itemId).orElseThrow(
-            () -> new CustomException(ErrorCode.INVALID_ITEM)
-        );
+    public void toggleHidden(Long userId, UserRole role, UUID itemId) {
+        Item item = checkValidRequestWithItem(userId, role, itemId);
+
         item.toggleIsHidden();
         itemRepository.save(item);
     }
@@ -128,5 +133,49 @@ public class ItemService {
     private void saveAiRequestHistory(Long userId, String request, String response) {
         AiRequestHistory requestHistory = new AiRequestHistory(userId, "gemini-2.5-flash", request, response);
         aiRequestHistoryRepository.save(requestHistory);
+    }
+
+    // Read, Update, Delete, Hide : 상품의 제어 권한 보유 여부 확인
+    private Item checkValidRequestWithItem(Long userId, UserRole role, UUID itemId) {
+        authorization(Set.of(UserRole.OWNER, UserRole.MANAGER, UserRole.MASTER), role);
+
+        Item item = itemRepository.findByItemIdWithNativeQuery(itemId).orElseThrow(
+            () -> new CustomException(ErrorCode.INVALID_ITEM)
+        );
+
+        Shop shop = shopRepository.findById(item.getShop().getId()).orElseThrow(
+            () -> new CustomException(ErrorCode.INVALID_SHOP)
+        );
+
+        isOwner(shop, userId, role);
+
+        return item;
+    }
+
+    // Create, ReadAll : 상품의 제어 권한 보유 여부 확인
+    private Shop checkValidRequestWithShop(Long userId, UserRole role, UUID shopId) {
+        authorization(Set.of(UserRole.OWNER, UserRole.MANAGER, UserRole.MASTER), role);
+
+        Shop shop = shopRepository.findById(shopId).orElseThrow(
+            () -> new CustomException(ErrorCode.INVALID_SHOP)
+        );
+
+        isOwner(shop, userId, role);
+
+        return shop;
+    }
+
+    private void authorization(Set<UserRole> validRoles, UserRole role) {
+        if (validRoles.contains(role)) {
+            return;
+        }
+        throw new CustomException(ErrorCode.FORBIDDEN_ACCESS);
+    }
+
+    private void isOwner(Shop shop, Long userId, UserRole role) {
+        if (role.equals(UserRole.MANAGER) || role.equals(UserRole.MASTER) || shop.getOwner().equals(userId)) {
+            return;
+        }
+        throw new CustomException(ErrorCode.FORBIDDEN_ACCESS);
     }
 }
--- src/main/java/com/delivery/justonebite/item/presentation/controller/ItemController.java
+++ src/main/java/com/delivery/justonebite/item/presentation/controller/ItemController.java
@@ -1,5 +1,6 @@
 package com.delivery.justonebite.item.presentation.controller;
 
+import com.delivery.justonebite.global.common.security.UserDetailsImpl;
 import com.delivery.justonebite.item.application.service.ItemService;
 import com.delivery.justonebite.item.presentation.dto.ItemDetailResponse;
 import com.delivery.justonebite.item.presentation.dto.ItemResponse;
@@ -13,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -26,60 +28,87 @@ public class ItemController {
 
     private final ItemService itemService;
 
-    // require fix : 추후 role과 같은 이슈 해결 필요 -> @AuthenticationPrincipal UserDetails userDetails
+    // 상품 CREATE
     @PostMapping
-    public ResponseEntity<ItemResponse> createItem(@RequestBody @Valid ItemRequest request) {
-        return ResponseEntity.status(HttpStatus.OK).body(itemService.createItem(request));
+    public ResponseEntity<ItemResponse> createItem(@RequestBody @Valid ItemRequest request,
+                                                   @AuthenticationPrincipal UserDetailsImpl user) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+            itemService.createItem(user.getUserId(), user.getUserRole(), request)
+        );
     }
 
+    // 숨김 및 삭제 상품 포함 단건 Read
     @GetMapping("/owner/{item-id}")
-    public ResponseEntity<ItemDetailResponse> getItemFromOwner(@PathVariable("item-id") UUID itemId) {
-        return ResponseEntity.status(HttpStatus.OK).body(itemService.getItemFromOwner(itemId));
+    public ResponseEntity<ItemDetailResponse> getItemFromOwner(@PathVariable("item-id") UUID itemId,
+                                                               @AuthenticationPrincipal UserDetailsImpl user) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+            itemService.getItemFromOwner(user.getUserId(), user.getUserRole(), itemId)
+        );
     }
 
+    // 숨김 및 삭제 상품 제외 단건 Read
     @GetMapping("/{item-id}")
     public ResponseEntity<ItemDetailResponse> getItemFromCustomer(@PathVariable("item-id") UUID itemId) {
-        return ResponseEntity.status(HttpStatus.OK).body(itemService.getItemFromCustomer(itemId));
+        return ResponseEntity.status(HttpStatus.OK).body(
+            itemService.getItemFromCustomer(itemId)
+        );
     }
 
-    @GetMapping("/shop/owner/{shop-id}") // owner 용, fix : 추후 권한에 따라서 다른 service를 쓰도록 할 것 같음
+    // 가게별 숨김 및 삭제 상품 포함 Read
+    @GetMapping("/owner/shop/{shop-id}") // owner 용, fix : 추후 권한에 따라서 다른 service를 쓰도록 할 것 같음
     public ResponseEntity<Page<ItemResponse>> getItemsByShopFromOwner(@PathVariable("shop-id") String shopId,
-                                                             @RequestParam(name = "page", defaultValue = "0") int page,
-                                                             @RequestParam(name = "size", defaultValue = "10") int size,
-                                                             @RequestParam(name = "sort-by", defaultValue = "createdAt") String sortBy) {
+                                                                      @RequestParam(name = "page", defaultValue = "0") int page,
+                                                                      @RequestParam(name = "size", defaultValue = "10") int size,
+                                                                      @RequestParam(name = "sort-by", defaultValue = "createdAt") String sortBy,
+                                                                      @AuthenticationPrincipal UserDetailsImpl user) {
         Pageable pageable = PageRequest.of(page, size, Sort.by(changeSnakeCase(sortBy)));
-        return ResponseEntity.status(HttpStatus.OK).body(itemService.getItemsByShop(UUID.fromString(shopId), pageable));
+        return ResponseEntity.status(HttpStatus.OK).body(
+            itemService.getItemsByShopFromOwner(user.getUserId(), user.getUserRole(), UUID.fromString(shopId), pageable)
+        );
     }
 
+    // 가게별 숨김 및 삭제 상품 제외 Read
     @GetMapping("/shop/{shop-id}") // customer 용
     public ResponseEntity<Page<ItemResponse>> getItemsByShopFromCustomer(@PathVariable("shop-id") String shopId,
                                                                           @RequestParam(name = "page", defaultValue = "0") int page,
                                                                           @RequestParam(name = "size", defaultValue = "10") int size,
                                                                           @RequestParam(name = "sort-by", defaultValue = "createdAt") String sortBy) {
         Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
-        return ResponseEntity.status(HttpStatus.OK).body(itemService.getItemsByShopWithoutHidden(UUID.fromString(shopId), pageable));
+        return ResponseEntity.status(HttpStatus.OK).body(
+            itemService.getItemsByShopFromCustomer(UUID.fromString(shopId), pageable)
+        );
     }
 
+    // 상품 Update
     @PutMapping("/{item-id}")
-    public ResponseEntity<ItemResponse> updateItem(@PathVariable("item-id") UUID itemId, @RequestBody @Valid ItemUpdateRequest request) {
-        return ResponseEntity.status(HttpStatus.OK).body(itemService.updateItem(itemId, request));
+    public ResponseEntity<ItemResponse> updateItem(@PathVariable("item-id") UUID itemId, @RequestBody @Valid ItemUpdateRequest request,
+                                                   @AuthenticationPrincipal UserDetailsImpl user) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+            itemService.updateItem(user.getUserId(), user.getUserRole(), itemId, request)
+        );
     }
 
+    // 상품 Soft Delete
     @DeleteMapping("/{item-id}")
-    public ResponseEntity<Void> softDelete(@PathVariable("item-id") String itemId) {
-        itemService.softDelete(1L, UUID.fromString(itemId));
+    public ResponseEntity<Void> softDelete(@PathVariable("item-id") String itemId,
+                                           @AuthenticationPrincipal UserDetailsImpl user) {
+        itemService.softDelete(user.getUserId(), user.getUserRole(), UUID.fromString(itemId));
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    // 상품 Restore
     @PatchMapping("/{item-id}/restore")
-    public ResponseEntity<Void> restoreItem(@PathVariable("item-id") String itemId) {
-        itemService.restoreItem(UUID.fromString(itemId));
+    public ResponseEntity<Void> restoreItem(@PathVariable("item-id") String itemId,
+                                            @AuthenticationPrincipal UserDetailsImpl user) {
+        itemService.restoreItem(user.getUserId(), user.getUserRole(), UUID.fromString(itemId));
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    // 상품 Hidden / Reveal
     @PatchMapping("/{item-id}/hide")
-    public ResponseEntity<Void> toggleHidden(@PathVariable("item-id") String itemId) {
-        itemService.toggleHidden(UUID.fromString(itemId));
+    public ResponseEntity<Void> toggleHidden(@PathVariable("item-id") String itemId,
+                                             @AuthenticationPrincipal UserDetailsImpl user) {
+        itemService.toggleHidden(user.getUserId(), user.getUserRole(), UUID.fromString(itemId));
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
