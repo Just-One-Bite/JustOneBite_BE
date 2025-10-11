30ae01ad05e95d5bee713c17afc79541a5979127
jessi87 <jessiseo87@gmail.com>
2025-10-11 14:34:15 +0900

    feat: Payment 엔티티 및 기본 구조 생성

--- src/main/java/com/delivery/justonebite/payment/domain/repository/PaymentRepository.java
+++ src/main/java/com/delivery/justonebite/payment/domain/repository/PaymentRepository.java
@@ -4,8 +4,8 @@ import com.delivery.justonebite.payment.domain.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 import java.util.UUID;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 }
