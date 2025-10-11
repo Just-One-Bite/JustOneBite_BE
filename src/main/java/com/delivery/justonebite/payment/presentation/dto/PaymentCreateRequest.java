00553fda3b26b6fc5bd040acffd41b2e3a0ea2b6
jessi87 <jessiseo87@gmail.com>
2025-10-11 14:48:59 +0900

    feat: Payment 엔티티 및 기본 구조 생성

---
+++ src/main/java/com/delivery/justonebite/payment/presentation/dto/PaymentCreateRequest.java
@@ -0,0 +1,16 @@
+package com.delivery.justonebite.payment.presentation.dto;
+
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+public class PaymentCreateRequest {
+    private UUID orderId;
+    private String orderName;
+    private UUID shopId;
+    private String method;
+    private BigDecimal totalAmount;
+
+}
