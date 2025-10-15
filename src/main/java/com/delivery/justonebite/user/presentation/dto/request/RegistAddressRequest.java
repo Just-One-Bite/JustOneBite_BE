4b396f9880845c24f7a545e2d958652835e653c5
thdwnsdlf12 <il4500@naver.com>
2025-10-15 16:21:47 +0900

    refactor: NotBlank 메세지 추가

--- src/main/java/com/delivery/justonebite/user/presentation/dto/request/RegistAddressRequest.java
+++ src/main/java/com/delivery/justonebite/user/presentation/dto/request/RegistAddressRequest.java
@@ -3,13 +3,13 @@ package com.delivery.justonebite.user.presentation.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record RegistAddressRequest(
-        @NotBlank()
+        @NotBlank(message = "대분류(시/도)를 입력해주세요")
         String province,
-        @NotBlank
+        @NotBlank(message = "증분류(시/군/구)를 입력해주세요")
         String city,
-        @NotBlank
+        @NotBlank(message = "소분류(읍/면/동)을 입력해주세요")
         String district,
-        @NotBlank
+        @NotBlank(message = "주소(도로명주소)를 입력해주세요")
         String address,
         boolean isDefault
 ) {
