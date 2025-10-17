d636218ec16e1f1d7c092d0725709e791b3a68db
WonJin Bae <78189431+try3982@users.noreply.github.com>
2025-10-17 12:34:36 +0900

    Revert "refactor(review) : 스케줄러 도입이후  리뷰가 새로 생성되거나 수정되면 수정된 리뷰만 평점 반영"

--- src/main/java/com/delivery/justonebite/review/application/service/ReviewAggregationService.java
+++ src/main/java/com/delivery/justonebite/review/application/service/ReviewAggregationService.java
@@ -1,14 +1,9 @@
 package com.delivery.justonebite.review.application.service;
 
-import com.delivery.justonebite.global.exception.custom.CustomException;
-import com.delivery.justonebite.global.exception.response.ErrorCode;
-import com.delivery.justonebite.order.domain.repository.OrderRepository;
 import com.delivery.justonebite.shop.domain.repository.ShopRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.UUID;
 
 
 @Service
@@ -16,23 +11,11 @@ import java.util.UUID;
 public class ReviewAggregationService {
 
     private final ShopRepository shopRepository;
-    private final OrderRepository orderRepository;
 
 
     @Transactional
     public void recomputeAllShopAvg() {
         int updated = shopRepository.bulkUpdateAllAvg();
-    }
-
-    @Transactional
-    public void updateShopAvg(UUID orderId) {
-        UUID shopId = orderRepository.findShopIdByOrderId(orderId)
-                .orElseThrow(() -> new CustomException(ErrorCode.ORDER_NOT_FOUND));
-
-        int updated = shopRepository.updateShopAvgById(shopId);
-        if (updated == 0) {
-            shopRepository.resetShopAvgToZero(shopId);
-        }
     }
 
 }
