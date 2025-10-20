271c1870da601b84cf8b90478e0670e6a4141b4b
thdwnsdlf12 <il4500@naver.com>
2025-10-20 16:48:29 +0900

    refactor: response Dto Swagger 문서화 적용

--- src/main/java/com/delivery/justonebite/user/presentation/dto/response/CreateManagerResponse.java
+++ src/main/java/com/delivery/justonebite/user/presentation/dto/response/CreateManagerResponse.java
@@ -2,21 +2,28 @@ package com.delivery.justonebite.user.presentation.dto.response;
 
 import com.delivery.justonebite.user.domain.entity.User;
 import com.delivery.justonebite.user.domain.entity.UserRole;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "관리자(MANAGER) 생성 응답 DTO")
 @Builder
 public record CreateManagerResponse(
+        @Schema(description = "관리자 고유 Id", example = "2")
         Long userId,
+        @Schema(description = "인증에 사용된 이메일", example = "manager1@email.com")
         String email,
+        @Schema(description = "관리자 이름", example = "111")
         String name,
+        @Schema(description = "관리자 전화번호", example = "010-1111-1111")
         String phoneNumber,
+        @Schema(description = "관리자가 가진 권한", example = "MANAGER")
         UserRole userRole,
+        @Schema(description = "생성일시", example = "2025:01:01T00:00:00")
         LocalDateTime createdAt,
-        Long createdBy,
-        LocalDateTime updatedAt,
-        Long updatedBy
+        @Schema(description = "관리자 계정을 생성한 생성자", example = "1")
+        Long createdBy
 ) {
     public static CreateManagerResponse toDto(User user) {
         return CreateManagerResponse.builder()
@@ -27,8 +34,6 @@ public record CreateManagerResponse(
                 .userRole(user.getUserRole())
                 .createdAt(user.getCreatedAt())
                 .createdBy(user.getCreatedBy())
-                .updatedAt(user.getUpdatedAt())
-                .updatedBy(user.getUpdatedBy())
                 .build();
     }
 }
--- src/main/java/com/delivery/justonebite/user/presentation/dto/response/LoginResponse.java
+++ src/main/java/com/delivery/justonebite/user/presentation/dto/response/LoginResponse.java
@@ -1,10 +1,14 @@
 package com.delivery.justonebite.user.presentation.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
+@Schema(description = "로그인 응답 DTO")
 @Builder
 public record LoginResponse(
+        @Schema(description = "인증에 필요한 Access Token", example = "{accessToken}")
         String accessToken,
+        @Schema(description = "Access Token 재발급에 필요한 Refresh Token", example = "{refreshToken}")
         String refreshToken
 ) {
     public static LoginResponse toDto(String accessToken, String refreshToken) {
